e85a74773e00393dca763b70d542957d8e73568e
Ian S. McBride <ian.s.mcb@gmail.com>
2016-10-15 22:26:55 -0400

    Add a basic web interface
    Interface uses Flask and a copy of the notebook code. The notebook was
    exported to a .py file.
    
    How to start the interface:
    $ cd ian
    $ pip install -r requirements.txt
    $ cd blindSearch
    $ python blindSearch.py
    
    Limitation:
    - Engine names are never hidden
    - Engines aren't randomized
    - No way for user to rate/guess engines
    - No html styling

---
+++ ian/blindSearch/blindSearch.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+from searchResults import yahoo, google, bing
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def landing():
+    return render_template('landing.html')
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('q', False)
+    if not query:
+        return redirect(url_for('landing'))
+
+    data = {
+        'query': query,
+        'engines': [
+            {'name': 'Bing'  , 'links': bing(query)},
+            {'name': 'Google', 'links': google(query)},
+            {'name': 'Yahoo' , 'links': yahoo(query)},
+        ],
+    }
+    return render_template('search.html', **data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
