7731811360a6f408c890cd3296a68bcc3a7e814a
Ian S. McBride <ian.s.mcb@gmail.com>
2016-10-17 21:39:14 -0400

    Add trial view/template

--- doubleBlindSearch/doubleBlindSearch/views.py
+++ doubleBlindSearch/doubleBlindSearch/views.py
@@ -2,6 +2,12 @@ from flask import render_template
 
 from . import app
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/trial')
+def trial():
+    return render_template('trial.html')
