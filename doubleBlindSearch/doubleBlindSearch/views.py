8616f9c2ef963fe44e3902ebd2af8dbb4aa396d2
Ian S. McBride <ian.s.mcb@gmail.com>
2016-10-18 17:58:06 -0400

    Update trial view/template
    Template now receives and displays the query.

--- doubleBlindSearch/doubleBlindSearch/views.py
+++ doubleBlindSearch/doubleBlindSearch/views.py
@@ -12,10 +12,14 @@ def index():
 @app.route('/trial', methods=('POST',))
 def trial():
     data = request.form.to_dict(flat=True)
+    query = data['query']
 
     # If form is valid
     if float(data['valueA']) != 0.0:
         # Process form
         data = updateMeansLabelsAndFields(data)
+    else:
+        # Display default field values
+        data = defaultFields
 
-    return render_template('trial.html', **data)
+    return render_template('trial.html', query=query, **data)
