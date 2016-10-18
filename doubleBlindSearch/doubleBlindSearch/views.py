0bc8987d2b66430789106f5893d8e57349d5ee1e
Ian S. McBride <ian.s.mcb@gmail.com>
2016-10-18 09:25:23 -0400

    Force index view to redirect to trial view using POST

--- doubleBlindSearch/doubleBlindSearch/views.py
+++ doubleBlindSearch/doubleBlindSearch/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, request, url_for
 
 from . import app
 from .forms import InitialSearchForm, TrialForm
@@ -8,15 +8,23 @@ from .forms import InitialSearchForm, TrialForm
 def index():
     form = InitialSearchForm()
     if form.validate_on_submit():
-        return redirect(url_for('trial'))
+        return redirect(url_for('trial'), code=307)
 
     return render_template('index.html', form=form)
 
 
-@app.route('/trial', methods=('GET', 'POST'))
+@app.route('/trial', methods=('POST',))
 def trial():
+    # Retrieve current query
+    try:
+        current_query = request.form['query']
+    except KeyError:
+        return redirect(url_for('index'))
+
     form = TrialForm()
+
+    # Advance to next trial (if form is valid)
     if form.validate_on_submit():
         return redirect('/success')
 
-    return render_template('trial.html', form=form)
+    return render_template('trial.html', form=form, current_query=current_query)
